bcbefc0c64ee414e3d2580127aec9bb501ccf951
zhuyu <zhuyu553@gmail.com>
2019-03-27 22:23:11 +0800

    创建数组方法，并实现数组的添加

--- Arrays/02-Create-Arrays/src/Arrays.java
+++ Arrays/02-Create-Arrays/src/Arrays.java
@@ -39,4 +39,34 @@ public class Arrays {
     public boolean isEmpty(){
         return size==0;
     }
+
+    /**
+     * 向所有元素后添加一个新元素
+     * @param e
+     */
+    public void addLast(int e){
+        add(size,e);
+    }
+    //向所有元素前添加一个新元素
+    public void addFirst(int e){
+        add(size,e);
+    }
+
+    /**
+     * 在第index个位置插入一个新元素
+     * @param index 插入位置
+     * @param e 插入元素
+     */
+    public void add(int index,int e){
+        if(size==data.length){
+            throw new IllegalArgumentException("Add failed.Array is full.");
+        }else if(index<0||size<index){
+            throw new IllegalArgumentException("Add failed.Require index>=0 and index<=size.");
+        }
+        for(int i=size-1;i>=index;i--){
+            data[i+1]=data[i];
+        }
+        data[index]=e;
+        size++;
+    }
 }
