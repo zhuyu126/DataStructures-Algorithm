73ac9ffe1b9971b58db21767d6d5b685f5d511e1
zhuyu <zhuyu553@gmail.com>
2019-03-28 18:37:03 +0800

    验证泛型数组

---
+++ Arrays/02-Create-Arrays/src/Student.java
@@ -0,0 +1,22 @@
+public class Student {
+    private String name;
+    private int score;
+
+    public Student(String studentName, int studentScore) {
+        this.name = studentName;
+        this.score = studentScore;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Student(name:%s,score:%d)",name,score);
+    }
+
+    public static void main(String[] args) {
+        Arrays<Student> array=new Arrays<>();
+        array.addLast(new Student("Alice",100));
+        array.addLast(new Student("Tom",90));
+        array.addLast(new Student("Ben",88));
+        System.out.println(array);
+    }
+}
