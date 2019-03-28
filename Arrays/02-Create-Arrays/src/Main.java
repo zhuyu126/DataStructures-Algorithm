f5ef89d281188c3c2504a8113cb6214b0ef70f5c
zhuyu <zhuyu553@gmail.com>
2019-03-28 17:04:34 +0800

    验证删除操作

--- Arrays/02-Create-Arrays/src/Main.java
+++ Arrays/02-Create-Arrays/src/Main.java
@@ -11,5 +11,14 @@ public class Main {
 
         arr.addFirst(-1);
         System.out.println(arr);
+
+        arr.remove(2);
+        System.out.println(arr);
+
+        arr.removeElement(4);
+        System.out.println(arr);
+
+        arr.removeFirst();
+        System.out.println(arr);
     }
 }
