70674932feeb0cc589054a381e37984f3a159b3a
zhuyu <zhuyu553@gmail.com>
2019-04-02 20:44:10 +0800

    LeetCode-20.有效括号

---
+++ Stack/02-Stack-Valid-Parentheses/src/Solution.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+public class Solution {
+    public boolean isValid(String s){
+        Stack<Character> stack=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(c=='('||c=='['||c=='{'){
+                stack.push(c);
+            }else {
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char topChar=stack.pop();
+                if (c==')'&& topChar!='('){
+                    return false;
+                }else if(c==']' && topChar!='['){
+                    return false;
+                }else if(c=='}' && topChar!='{'){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
