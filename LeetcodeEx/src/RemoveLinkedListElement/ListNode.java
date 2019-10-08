1229e6596a9fe5972130910b05dbebbb6bc1bb06
robinson <zhuyu553@gmail.com>
2019-10-08 18:09:18 +0800

    递归解决方法

--- LeetcodeEx/src/RemoveLinkedListElement/ListNode.java
+++ LeetcodeEx/src/RemoveLinkedListElement/ListNode.java
@@ -18,9 +18,9 @@ public class ListNode {
 
       public ListNode(int[] arr){
 
-            if(arr == null || arr.length == 0)
-                  throw new IllegalArgumentException("arr can not be empty");
-
+            if(arr == null || arr.length == 0) {
+                  throw new IllegalArgumentException( "arr can not be empty" );
+            }
             this.val = arr[0];
             ListNode cur = this;
             for(int i = 1 ; i < arr.length ; i ++){
