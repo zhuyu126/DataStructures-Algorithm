1f03e2eee84f79e45b12af84f2ef8773eb9d680a
robinson <zhuyu553@gmail.com>
2020-01-09 15:48:56 +0800

    层次遍历操作说明

--- 05-BinarySearchTree/01-BST/src/Mains.java
+++ 05-BinarySearchTree/01-BST/src/Mains.java
@@ -4,7 +4,7 @@
 public class Mains {
     public static void main(String[] args) {
         BST<Integer> bst=new BST<>();
-        int[] nums={5,3,7,9,4,2};
+        int[] nums={5,3,7,9,4,2,};
         for(int num:nums){
             bst.add(num);
         }
