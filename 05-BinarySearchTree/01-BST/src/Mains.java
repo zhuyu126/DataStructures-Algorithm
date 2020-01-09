87272a77ca5db922c7fb8939cadec2980454820c
robinson <zhuyu553@gmail.com>
2020-01-09 18:04:26 +0800

    删除最小最大元素的节点既正确性验证

--- 05-BinarySearchTree/01-BST/src/Mains.java
+++ 05-BinarySearchTree/01-BST/src/Mains.java
@@ -1,33 +1,37 @@
+import java.util.Random;
+
 /**
  * BST测试用例代码
  */
 public class Mains {
     public static void main(String[] args) {
         BST<Integer> bst=new BST<>();
-        int[] nums={5,3,7,9,4,2};
+        Random random=new Random();
+        int n=10;
+        //int[] nums={5,3,7,9,4,2};
 //        int[] nums={41,22,15,13,33,37,58,50,42,53};
-        for(int num:nums){
-            bst.add(num);
+        for(int i=0;i<10;i++){
+            bst.add(random.nextInt(100));
         }
         bst.preOrder();
-        System.out.println();
-        bst.preOrderNR();
-
+//        System.out.println();
+//        bst.preOrderNR();
+//
         System.out.println();
         System.out.println(bst);
-        System.out.println();
-        bst.inOrder();
-        System.out.println();
-        bst.inOrderNR();
-        System.out.println();
-        bst.postOrder();
-        System.out.println();
-        bst.postOrderNR();
-        System.out.println();
-        bst.levelOrder();
-        System.out.println(bst.maxmun());
-        System.out.println(bst.minimum());
-        bst.removeMin();
-        System.out.println(bst);
+//        System.out.println();
+//        bst.inOrder();
+//        System.out.println();
+//        bst.inOrderNR();
+//        System.out.println();
+//        bst.postOrder();
+//        System.out.println();
+//        bst.postOrderNR();
+//        System.out.println();
+//        bst.levelOrder();
+//        System.out.println(bst.maxmun());
+//        System.out.println(bst.minimum());
+//        bst.removeMin();
+//        System.out.println(bst);
     }
 }
