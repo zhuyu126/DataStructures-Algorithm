c587ff0e5242aa44c8d952b6e35ac69c6b8c0a8e
robinson <zhuyu553@gmail.com>
2020-03-03 13:19:14 +0800

    MaxHeap简单的正确性验证

--- 07-HeapAndPriorityQueue/01-MaxHeap/MaxHeapTest.java
+++ 07-HeapAndPriorityQueue/01-MaxHeap/MaxHeapTest.java
@@ -10,10 +10,10 @@ public class MaxHeapTest {
             maxHeap.add(random.nextInt(Integer.MAX_VALUE));
         }
         int[] arr=new int[n];
-        for (int i=0;n<n;i++){
+        for (int i=0;i<n;i++){
             arr[i]=maxHeap.extractMax();
         }
-        for (int i=1;i<n-1;i++){
+        for (int i=1;i<n;i++){
             if(arr[i-1]<arr[i]){
                 throw new IllegalArgumentException("Error!");
 
