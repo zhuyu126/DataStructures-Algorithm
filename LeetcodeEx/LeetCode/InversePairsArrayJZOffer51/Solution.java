cf450e2666d58e25c12d0eaf430a3c62d89945a6
robinson <zhuyu553@gmail.com>
2020-08-07 15:37:17 +0800

    Double loop but Time Out

---
+++ LeetcodeEx/LeetCode/InversePairsArrayJZOffer51/Solution.java
@@ -0,0 +1,29 @@
+package InversePairsArrayJZOffer51;
+
+/**
+ * https://leetcode-cn.com/problems/shu-zu-zhong-de-ni-xu-dui-lcof/
+ */
+
+/**
+ * 双指针解法
+ * leetcode 测试超时
+ */
+class Solution {
+    public int reversePairs(int[] nums) {
+        int res=0;
+        for (int i=0;i<nums.length;i++){
+            for (int j=i+1;j<nums.length;j++){
+                if (nums[i]>nums[j]){
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums=new int[]{7,5,6,4};
+        Solution sl=new Solution();
+        System.out.println(sl.reversePairs(nums));
+    }
+}
