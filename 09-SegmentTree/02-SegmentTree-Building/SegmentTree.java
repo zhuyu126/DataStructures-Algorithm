746d80797cc6edda58706d87f8143095be05d293
zhuyu <zhuyu553@gmail.com>
2020-11-09 12:56:59 +0800

    Realization of SegmentTree By Static Array

--- 09-SegmentTree/02-SegmentTree-Building/SegmentTree.java
+++ 09-SegmentTree/02-SegmentTree-Building/SegmentTree.java
@@ -76,6 +76,36 @@ public class SegmentTree<E> {
         }
     }
 
+    /**
+     * 将index位置的值，更新为e
+     * @param index 更新的位置
+     * @param e 更新的值
+     */
+    public void set(int index,E e){
+        if(index < 0 || index >= data.length){
+            throw new IllegalArgumentException("Index is illegal");
+        }
+        data[index]=e;
+        set(0,0,data.length-1,index,e);
+    }
+
+    private void set(int treeIndex, int l, int r, int index, E e) {
+        if (l==r){
+            tree[treeIndex]=e;
+            return;
+        }
+        int mid=l+(r-l)/2;
+
+        int leftTreeIndex=leftChild(treeIndex);
+        int rightTreeIndex=rightChild(treeIndex);
+        if (index>=mid+1){
+            set(rightTreeIndex,mid+1,r,index,e);
+        }else {
+            set(leftTreeIndex,l,mid,index,e);
+        }
+        tree[treeIndex]=merger.merger(tree[leftTreeIndex],tree[rightTreeIndex]);
+    }
+
     public int getSize(){
         return data.length;
     }
