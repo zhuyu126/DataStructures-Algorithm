d01b60fb950fb8b23239757d01657555eb8606aa
zhuyu <zhuyu553@gmail.com>
2020-11-11 10:46:05 +0800

    Realization of UnionFind By Tree

--- 11-UnionFind/02-QuickUnion/QuickUnion.java
+++ 11-UnionFind/02-QuickUnion/QuickUnion.java
@@ -18,20 +18,38 @@ public class QuickUnion implements UF {
         return parent.length;
     }
 
+    /**
+     * 查看元素p和元素q是否所属一个集合 O(h)复杂度, h为树的高度
+     * @param p
+     * @param q
+     * @return
+     */
     @Override
     public boolean isConnected(int p, int q) {
         return find(p)==find(q);
     }
+
+    /**
+     * 查找过程, 查找元素p所对应的集合编号 O(h)复杂度, h为树的高度
+     * @param p
+     * @return
+     */
     private int find(int p){
         if(p < 0 || p >= parent.length) {
             throw new IllegalArgumentException("p is out of bound.");
         }
+        //不断去查询自己的父亲节点, 直到到达根节点 根节点的特点: parent[p] == p
         while (p!=parent[p]){
             p=parent[p];
         }
         return p;
     }
 
+    /**
+     * 合并元素p和元素q所属的集合 O(h)复杂度, h为树的高度
+     * @param p
+     * @param q
+     */
     @Override
     public void unionElements(int p, int q) {
         int pRoot=find(p);
