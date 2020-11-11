8c88ba3fcc2d5ef0ed65b4484c6da2b93a31b635
zhuyu <zhuyu553@gmail.com>
2020-11-11 11:04:13 +0800

    Compare the Performance of QuickFind And QuickUnion

---
+++ 11-UnionFind/02-QuickUnion/Main.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+public class Main {
+    private static double testUF(UF uf,int m){
+        int size=uf.getSize();
+        Random random=new Random();
+        long startTime=System.nanoTime();
+        for (int i=0;i<m;i++){
+            int a=random.nextInt(size);
+            int b=random.nextInt(size);
+            uf.unionElements(a,b);
+        }
+        for (int i=0;i<m;i++){
+            int a=random.nextInt(size);
+            int b=random.nextInt(size);
+            uf.isConnected(a,b);
+        }
+        long endTime=System.nanoTime();
+        return (endTime-startTime)/1000000000.0;
+    }
+    public static void main(String[] args) {
+
+        //QuickFind 慢于 QuickUnion
+//        int size=100000;
+//        int m=10000;
+        //QuickUnion 慢于 QuickFind
+        int size=100000;
+        int m=100000;
+        QuickFind quickFind=new QuickFind(size);
+        System.out.println("QuickFind : " + testUF(quickFind, m) + " s");
+        QuickUnion quickUnion=new QuickUnion(size);
+        System.out.println("QuickUnion : " + testUF(quickUnion, m) + " s");
+
+    }
+}
