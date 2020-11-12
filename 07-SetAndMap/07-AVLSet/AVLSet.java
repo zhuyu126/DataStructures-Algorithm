a7084f9054240870f04a847bdafaa7ac2c625730
zhuyu <zhuyu553@gmail.com>
2020-11-12 13:28:12 +0800

    Realization of Map and Set in AVLTree

---
+++ 07-SetAndMap/07-AVLSet/AVLSet.java
@@ -0,0 +1,32 @@
+public class AVLSet<E extends Comparable<E>>implements Set<E> {
+    private AVLTree<E,Object> avlTree;
+
+    public AVLSet() {
+        avlTree=new AVLTree<>();
+    }
+
+    @Override
+    public void add(E e) {
+        avlTree.add(e,null);
+    }
+
+    @Override
+    public void remove(E e) {
+        avlTree.remove(e);
+    }
+
+    @Override
+    public boolean contains(E e) {
+        return avlTree.contains(e);
+    }
+
+    @Override
+    public int getSize() {
+        return avlTree.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return avlTree.isEmpty();
+    }
+}
